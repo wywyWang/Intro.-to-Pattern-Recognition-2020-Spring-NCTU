70d5458b48e4261beeda6e4cbf9360a427c5b043
yao0510 <sf163864@gmail.com>
2020-04-21 12:34:34 +0800

    modify coding style to fit PEP8

--- HW2/0516075_HW2.py
+++ HW2/0516075_HW2.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 CLASS = 2
 K = 7
@@ -26,35 +28,38 @@ def compute_mean(x_train, y_train):
 def compute_withinclass(x_train, y_train, class_mean):
     within_class = np.zeros([x_train.shape[1], x_train.shape[1]])
     for data_idx in range(x_train.shape[0]):
-        dist = np.subtract(x_train[data_idx], class_mean[y_train[data_idx]]).reshape(x_train.shape[1], 1)
+        dist = np.subtract(x_train[data_idx], class_mean[y_train[data_idx]])
+        dist = dist.reshape(x_train.shape[1], 1)
         within_class += np.matmul(dist, dist.T)
     print("Within-class scatter matrix SW: {}".format(within_class))
     return within_class
 
 
 def compute_betweenclass(class_mean):
-    dist = np.subtract(class_mean[0], class_mean[1]).reshape(class_mean.shape[1], 1)
+    dist = np.subtract(class_mean[0], class_mean[1])
+    dist = dist.reshape(class_mean.shape[1], 1)
     between_class = np.matmul(dist, dist.T)
     print("Between-class scatter matrix SB: {}".format(between_class))
     return between_class
 
 
 def compute_eigen(A):
-    eigenvalues, eigenvectors = np.linalg.eigh(A)
+    eigenvalues, eigenvectors = np.linalg.eig(A)
     idx = eigenvalues.argsort()[::-1]                          # sort largest
-    weight = eigenvectors[:,idx][:,:1]
+    weight = eigenvectors[:, idx][:, :1]
     print("Fisher’s linear discriminant: {}".format(weight))
     return weight
 
 
-def k_nearest_neighbor(x_train, y_train, x_test):
+def knn(x_train, y_train, x_test):
     y_pred = []
     train_size = x_train.shape[0]
     test_size = x_test.shape[0]
     for test_idx in range(test_size):
         all_dist = np.zeros(train_size)
         for train_idx in range(train_size):
-            dist = np.sqrt(np.sum((x_test[test_idx] - x_train[train_idx]) ** 2))
+            partial_dist = (x_test[test_idx] - x_train[train_idx]) ** 2
+            dist = np.sqrt(np.sum(partial_dist))
             all_dist[train_idx] = dist
         sort_idx = all_dist.argsort()
         neighbor = list(y_train[sort_idx][:K])
@@ -77,8 +82,9 @@ if __name__ == "__main__":
     class_mean = compute_mean(x_train, y_train)
     within_class = compute_withinclass(x_train, y_train, class_mean)
     between_class = compute_betweenclass(class_mean)
-    weight = compute_eigen(np.matmul(np.linalg.pinv(within_class), between_class))
+    Sw_inverse_Sb = np.matmul(np.linalg.pinv(within_class), between_class)
+    weight = compute_eigen(Sw_inverse_Sb)
     lower_dimension_train = np.matmul(x_train, weight)
     lower_dimension_test = np.matmul(x_test, weight)
-    y_pred = k_nearest_neighbor(lower_dimension_train, y_train, lower_dimension_test)
+    y_pred = knn(lower_dimension_train, y_train, lower_dimension_test)
     compute_accuracy(y_pred, y_test)
